796e910d11a343301fe91486014d5e42ce666445
Manbir Sandhu <mana24129@hotmail.com>
2020-02-24 16:15:27 -0700

    fixed main app

--- main/src/mainApp.java
+++ main/src/mainApp.java
@@ -61,6 +61,111 @@ public class mainApp {
 	private void UpdateUser() 
 	{
 		displayUpdateUserPrompt();
+		System.out.println("Enter an option you would like to update: ");
+		System.out.println("1: Name");
+		System.out.println("2: Year of Study");
+		System.out.println("3: Major");
+		System.out.println("4: Minor");
+		System.out.println("5: Interests");
+		System.out.println("6: Faculty");
+		System.out.println();
+		
+		Scanner scan = new Scanner(System.in);
+		int option = scan.nextInt();
+		
+		switch (option) {
+		case 1:
+			Scanner name = new Scanner(System.in);
+			System.out.println("Enter Name: ");
+			String newName = name.nextLine();
+			currentPerson.setName(newName);
+		break;
+		
+		case 2:
+			Scanner YOS = new Scanner(System.in);
+			System.out.println("Enter Year of Study: ");
+			int newYOS = YOS.nextInt();
+			currentPerson.setYOS(newYOS);
+		
+			break;
+			
+		case 3: 
+			Scanner major = new Scanner(System.in);
+			System.out.println("Enter Major: ");
+			String newMajor = major.nextLine();
+			currentPerson.setMajor(newMajor);
+			break;
+			
+		case 4:
+			Scanner minor = new Scanner(System.in);
+			System.out.println("Enter Minor: ");
+			String newMinor = minor.nextLine();
+			currentPerson.setMinor(newMinor);
+			break;
+
+			
+		case 5:
+			Scanner interests = new Scanner(System.in);
+			System.out.println("Do you want to add or remove interests? (add/rem): ");
+			String response = interests.nextLine();
+			String newInterest;
+			String remInterest;
+			if (response.equals("add")) {
+				do {
+				System.out.println("Enter interest(s) to add: ");
+				newInterest = interests.nextLine();	
+				currentPerson.addInterest(newInterest);
+				}
+				while(!newInterest.contentEquals("")); 
+				
+			}
+				
+			if (response.equals("rem")) {
+				do {
+					System.out.println("Enter interest(s) to remove: ");
+					remInterest = interests.nextLine();
+					currentPerson.removeInterest(remInterest);
+						
+				}while(!remInterest.contentEquals(""));
+				
+			}
+			
+	
+			
+			break;
+			
+			
+		case 6:
+			Scanner fac = new Scanner(System.in);
+			System.out.println("Do you want to add or remove a faculty? (add/rem): ");
+			String response2 = fac.nextLine();
+			String newFac;
+			String remFac;
+			if (response2.equals("add")) {
+				do {
+				System.out.println("Enter faculty to add: ");
+				newFac = fac.nextLine();	
+				currentPerson.addFaculty(newFac);
+				}
+				while(!newFac.contentEquals("")); 
+				
+			}
+				
+			if (response2.equals("rem")) {
+				do {
+					System.out.println("Enter faculty to remove: ");
+					remFac = fac.nextLine();
+					currentPerson.removeInterest(remFac);
+						
+				}while(!remFac.contentEquals(""));
+				
+			}
+			
+	
+			
+			break;
+		}
+		
 		
 	}
 	
