5930860f57a143e275e2bdc3b98d09ea1f1113e8
Manbir Sandhu <mana24129@hotmail.com>
2020-02-27 14:15:25 -0700

    Added parent person class

---
+++ main/src/MasterPerson.java
@@ -0,0 +1,71 @@
+
+public class MasterPerson {
+	//Name of the person
+	private String name;
+	
+	//Age of the person
+	private int yos;
+	
+	// Major program the person is in
+	private String major;
+	
+	// Minor program the person is in
+	private String minor;
+	
+	public MasterPerson(String name, int yos, String major, String minor) 
+	{
+		this.name = new String(name);
+		this.yos = yos;
+		this.major = new String(major);
+		this.minor = new String(minor);
+	}
+	
+	public String getName() {
+		return new String(name);
+		
+	}
+	
+	public int getYOS() {
+		return yos;
+	
+	}
+	
+	public String getMajor() {
+		return new String(major);
+		
+	}
+	
+	public String getMinor() {
+		return new String(minor);
+		
+	}
+	
+	
+	public void setName(String newName) {
+		name = new String(newName);
+		
+	}
+
+	public void setYOS(int yos) {
+		this.yos = yos;
+		
+	}
+	
+	public void setMajor(String newMajor) {
+		major = new String(newMajor);
+		
+	}
+	
+	public void setMinor(String newMinor) {
+		minor = new String(newMinor);
+	}
+	
+	public void displayInfo() 
+	{
+		System.out.println("Name: " + name);
+		System.out.println("Year of Study: " + yos);
+		System.out.println("Major: " + major);
+	}
+	
+	
+}
