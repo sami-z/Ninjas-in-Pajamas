071401f891556eeb4098777304b611c0135e8e2b
Jake Kim <jakekim@Jakes-MacBook-Pro.local>
2020-02-29 21:37:52 -0700

    add monthly stuff to FinancePerson.java

--- main/src/FinancePerson.java
+++ main/src/FinancePerson.java
@@ -3,17 +3,21 @@ import java.util.*;
 public class FinancePerson extends MasterPerson{
 	
 	private double tuition;
-	private ArrayList<Double> allExpenses = new ArrayList<Double>();
+	private ArrayList<Double> allWeeklyExpenses = new ArrayList<Double>();
+	private ArrayList<Double> allMonthlyExpenses = new ArrayList<Double>();
 	private double longTermGoal;
 	private double weeklyIncome;
 	private double weeklyBudget;
 	
-	public FinancePerson(String name, int yos, String major, String minor, double tuition, ArrayList<Double> allExpenses, double longTermGoal, double weeklyIncome, double weeklyBudget)
+	public FinancePerson(String name, int yos, String major, String minor, double tuition, ArrayList<Double> allWeeklyExpenses, ArrayList<Double> allMonthlyExpenses, double longTermGoal, double weeklyIncome, double weeklyBudget)
 	{
 		super(name, yos, major, minor);
 		setTuition(tuition);
-		for (double d : allExpenses) {
-			addExpense(new Double(d));
+		for (double d : allWeeklyExpenses) {
+			addWeeklyExpense(new Double(d));
+		}
+		for (double d : allMonthlyExpenses) {
+			addMonthlyExpense(new Double(d));
 		}
 		setLongTermGoal(longTermGoal);
 		setWeeklyIncome(weeklyIncome);
@@ -60,22 +64,41 @@ public class FinancePerson extends MasterPerson{
 		this.weeklyBudget = weeklyBudget;
 	}
 	
-	public ArrayList<Double> getAllExpenses()
+	public ArrayList<Double> getAllWeeklyExpenses()
 	{
-		ArrayList<Double> newAllExpenses = new ArrayList<Double>();
-		for (Double d : allExpenses) {
-			newAllExpenses.add(new Double(d));
+		ArrayList<Double> newAllWeeklyExpenses = new ArrayList<Double>();
+		for (Double d : allWeeklyExpenses) {
+			newAllWeeklyExpenses.add(new Double(d));
 		}
-		return newAllExpenses;
+		return newAllWeeklyExpenses;
 	}
 	
-	public void addExpense(Double newExpense)
+	public void addWeeklyExpense(Double newExpense)
 	{
-		allExpenses.add(new Double(newExpense));
+		allWeeklyExpenses.add(new Double(newExpense));
 	}
 	
-	public void removeExpense(Double unwantedExpense)
+	public void removeWeeklyExpense(Double unwantedExpense)
 	{
-		allExpenses.remove(new Double(unwantedExpense));
+		allWeeklyExpenses.remove(new Double(unwantedExpense));
+	}
+	
+	public ArrayList<Double> getAllMonthlyExpenses()
+	{
+		ArrayList<Double> newAllMonthlyExpenses = new ArrayList<Double>();
+		for (Double d : allMonthlyExpenses) {
+			newAllMonthlyExpenses.add(new Double(d));
+		}
+		return newAllMonthlyExpenses;
+	}
+	
+	public void addMonthlyExpense(Double newExpense)
+	{
+		allMonthlyExpenses.add(new Double(newExpense));
+	}
+	
+	public void removeMonthlyExpense(Double unwantedExpense)
+	{
+		allMonthlyExpenses.remove(new Double(unwantedExpense));
 	}
 }
--- main/src/FinancialCalculator.java
+++ main/src/FinancialCalculator.java
@@ -2,8 +2,10 @@ import java.util.*;
 
 public class FinancialCalculator extends FinancePerson {
 	
-	public FinancialCalculator(String name, int yos, String major, String minor, double tuition, ArrayList<Double> allExpenses, double longTermGoal, double weeklyIncome, double weeklyBudget)
+	public FinancialCalculator(String name, int yos, String major, String minor, double tuition, ArrayList<Double> allWeeklyExpenses, ArrayList<Double> allMonthlyExpenses, double longTermGoal, double weeklyIncome, double weeklyBudget)
 	{
-		super(name, yos, major, minor, tuition, allExpenses, longTermGoal, weeklyIncome, weeklyBudget);
+		super(name, yos, major, minor, tuition, allWeeklyExpenses, allMonthlyExpenses, longTermGoal, weeklyIncome, weeklyBudget);
 	}
+	
+	
 }
