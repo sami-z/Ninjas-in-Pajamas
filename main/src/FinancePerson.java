7b68316124c5c148921fbfef8e84f927fae04de6
Jake Kim <jakekim@Jakes-MacBook-Pro.local>
2020-02-29 21:06:25 -0700

    finished FinancePerson.java

--- main/src/FinancePerson.java
+++ main/src/FinancePerson.java
@@ -3,7 +3,7 @@ import java.util.*;
 public class FinancePerson extends MasterPerson{
 	
 	private double tuition;
-	private ArrayList<Double> allExpenses;
+	private ArrayList<Double> allExpenses = new ArrayList<Double>();
 	private double longTermGoal;
 	private double weeklyIncome;
 	private double weeklyBudget;
@@ -11,8 +11,68 @@ public class FinancePerson extends MasterPerson{
 	public FinancePerson(String name, int yos, String major, String minor, double tuition, ArrayList<Double> allExpenses, double longTermGoal, double weeklyIncome, double weeklyBudget)
 	{
 		super(name, yos, major, minor);
-		
+		setTuition(tuition);
+		for (double d : allExpenses) {
+			addExpense(new Double(d));
+		}
 	}
 	
+	public double getTuition()
+	{
+		return this.tuition;
+	}
 	
+	public void setTuition(double tuition)
+	{
+		this.tuition = tuition;
+	}
+	
+	public double getLongTermGoal()
+	{
+		return this.longTermGoal;
+	}
+	
+	public void setLongTermGoal(double longTermGoal)
+	{
+		this.longTermGoal = longTermGoal;
+	}
+	
+	public double getWeeklyIncome()
+	{
+		return this.weeklyIncome;
+	}
+	
+	public void setWeeklyIncome(double weeklyIncome)
+	{
+		this.weeklyIncome = weeklyIncome;
+	}
+	
+	public double getWeeklyBudget()
+	{
+		return this.weeklyBudget;
+	}
+	
+	public void setWeeklyBudget(double weeklyBudget)
+	{
+		this.weeklyBudget = weeklyBudget;
+	}
+	
+	public ArrayList<Double> getAllExpenses()
+	{
+		ArrayList<Double> newAllExpenses = new ArrayList<Double>();
+		for (Double d : allExpenses) {
+			newAllExpenses.add(new Double(d));
+		}
+		return newAllExpenses;
+	}
+	
+	public void addExpense(Double newExpense)
+	{
+		allExpenses.add(new Double(newExpense));
+	}
+	
+	public void removeExpense(Double unwantedExpense)
+	{
+		allExpenses.remove(new Double(unwantedExpense));
+	}
 }
