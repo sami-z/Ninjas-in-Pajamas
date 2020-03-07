c645af94deedfa101b51dffc053b25d9abafa9cb
unknown <samizeremariam@gmail.com>
2020-03-07 13:39:47 -0700

    Basic Outline of GUI

---
+++ main/src/GUI_PACKAGE/Main.java
@@ -0,0 +1,26 @@
+package GUI_PACKAGE;
+	
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.stage.Stage;
+import javafx.scene.Scene;
+import javafx.scene.layout.AnchorPane;
+
+
+public class Main extends Application {
+	@Override
+	public void start(Stage primaryStage) {
+		try {
+			AnchorPane mainPane = (AnchorPane) FXMLLoader.load(getClass().getClassLoader().getResource("GUI.fxml"));
+			//Scene scene = new Scene(root,400,400);
+			primaryStage.setScene(new Scene (mainPane));
+			primaryStage.show();
+		} catch(Exception e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public static void main(String[] args) {
+		launch(args);
+	}
+}
