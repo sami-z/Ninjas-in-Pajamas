0b76cd8575305f26852ad8cc3a5850128b5748e7
Mackenzie Bowal <mackenziebowal@Mackenzies-MacBook-Air.local>
2020-03-11 22:20:03 -0600

    Created profile GUI

---
+++ main/src/PROFILE/ProfileController.java
@@ -0,0 +1,17 @@
+package PROFILE;
+
+import java.io.IOException;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.stage.Stage;
+
+public class ProfileController {
+	
+		
+	
+}
