2f49a657fd2b640e3e0cca40079eca0697cf6fb1
MBowal <60671297+MBowal@users.noreply.github.com>
2020-04-12 17:01:50 -0600

    documented MainApp

--- main/src/Backend/MainApp.java
+++ main/src/Backend/MainApp.java
@@ -1,12 +1,16 @@
 package Backend;
 import java.util.Scanner;
 
-public interface MainApp {
+/**
+ * This interface is implemented by FinanceApp, GPAApp, and ClubApp. 
+ * It consists of two methods: displayMenu() and performAction(int), 
+ * which are implemented by the other classes. 
+ */
 
+public interface MainApp {
+	
 	public void displayMenu();
 	
 	public boolean performAction(int action);
-	
-
 
 } 
