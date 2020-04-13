9b8a363674d72a6d456d75ed98a5fb015edd7a11
Manbir Sandhu <mana24129@hotmail.com>
2020-04-13 14:27:50 -0600

    Started documentation on Profile classes

--- main/src/PROFILE/ClubInfoProfileController.java
+++ main/src/PROFILE/ClubInfoProfileController.java
@@ -26,39 +26,72 @@ import javafx.scene.text.Text;
 import javafx.stage.Stage;
 
 public class ClubInfoProfileController {
+	
+	// The ArrayList that stores all of the interests
 	private ArrayList<String> interests = new ArrayList<String>();
+	
+	// The ArrayList that stores all of the faculties
 	private ArrayList<String> fac = new ArrayList<String>();
 	
-	Scene PreviousScene;
+	// The Scene object which contains the previous scene
+	private Scene PreviousScene;
 	
+	// VBox that is used to display the interests the user can pick from
 	@FXML
 	private VBox listOfInterests = new VBox();
 	
+	//The Stage object of the currentStage the scene is on
 	private Stage currentStage;
 	
+	// The Button Object for the back button
     @FXML
     private Button BackButtonOnClick;
 
+    
+	/**
+     * This method handles the event when the back button is clicked, it returns to the previous scene
+     * 
+     * @param event - An ActionEvent object that is passed into the method when the BackButton is clicked
+     */	
     @FXML
     void BackButtonOnClick(ActionEvent event)throws Exception {
     	currentStage.setScene(PreviousScene);
     	currentStage.show();
     }
-	
+    
+    
+	/** Returns the interests the user has
+	 * 
+	 * @return - A copy of the interests ArrayList
+	 */
 	public ArrayList<String> getInterests() 
 	{
 		return new ArrayList<String>(interests);
 	}
 	
+	/** Returns the faculties the user is interested
+	 * 
+	 * @return - A copy of the faculties ArrayList
+	 */
 	public ArrayList<String>getFaculties()
 	{
 		return new ArrayList<String>(fac);
 	}
 	
+	
+	/**
+	 * This method is used to create the interest and display the interest
+	 * 
+	 * @param mapping - The key of the interest (Ex 1.1)
+	 * @param text - The raw text of the interest (Ex Faculty of Arts)
+	 */
 	public void createInterest(String mapping, String text) 
 	{
+		// Creates an HBox which will list one interest
 		HBox root = new HBox();
 		root.setPadding(new Insets(10, 10, 10, 10));
+		
+		// Adds that HBox to the VBox listOfInterests so that it will be displayed in the scene
 		listOfInterests.getChildren().add(root);
 		
     	Label c = new Label(text);
@@ -67,15 +100,23 @@ public class ClubInfoProfileController {
         final Pane spacer = new Pane();
         HBox.setHgrow(spacer, Priority.ALWAYS);
         spacer.setMinSize(10, 1);
+        
+        //Two buttons are made for adding and removing an interest
         final Button add = new Button("Add");
         final Button del = new Button("Remove");
+        
+        // The "Remove" button is disabled because you first need to add the interest before you remove it
         del.setDisable(true);
+        
+        
         add.setOnAction(new EventHandler<ActionEvent>() 
         {
+        	// This method handles the event when the Add button is clicked
         	@Override
         	public void handle(ActionEvent event)
         	{
         		
+        		// Add an interest and disable the add button and enable the remove button
         		addInterest(mapping);
         		add.setDisable(true);
         		del.setDisable(false);
@@ -86,9 +127,11 @@ public class ClubInfoProfileController {
 
         del.setOnAction(new EventHandler<ActionEvent>() 
         {
+        	// This method handles the event when the Add button is clicked
         	@Override
         	public void handle(ActionEvent event)
         	{
+        		// Remove the interest and disable the remove button and enable the add button
         		removeInterest(mapping);
         		add.setDisable(false);
         		del.setDisable(true);
@@ -96,26 +139,49 @@ public class ClubInfoProfileController {
         });
         del.setMinSize(Button.USE_PREF_SIZE, Button.USE_PREF_SIZE);
         
+        // Adds the label, spacer and button to the HBox
         root.getChildren().addAll(c, spacer, add,del);
 	}
 	
+	/**
+	 * This method adds an interest to the interests ArrayList
+	 * 
+	 * @param key - The interest that is to be added
+	 */
 	public void addInterest(String key) 
 	{
+		// If the interest is not already in the ArrayList than add the interest
 		if(!interests.contains(key))
 			interests.add(new String(key));
 		System.out.println(interests.toString());
 	}
 	
+	/**
+	 * This function removes an interest from the interests ArrayList
+	 * 
+	 * @param toRemove The interest that is to be removed
+	 */
 	public void removeInterest(String toRemove) 
 	{
 		interests.remove(toRemove);
 	}
 	
+    /**
+     * Setter method that is used to pass in the previous stage
+     * 
+     * @param c - The stage that the caller is passing in to the controller
+     */
 	public void setStage(Stage c) 
 	{
 		currentStage = c;
 	}
 	
+	
+    /**
+     * Setter method that is used to set the previousScene instance variable
+     * 
+     * @param c - The previous scene that you want to go back to
+     */
 	public void setPreviousScene(Scene c) 
 	{
 		PreviousScene = c;
--- main/src/PROFILE/ProfileController.java
+++ main/src/PROFILE/ProfileController.java
@@ -27,80 +27,89 @@ import FNCECALC.FinanceCalculatorController;
 
 public class ProfileController {
 	
-	ObservableList<String> YOSlist = FXCollections.observableArrayList("1", "2", "3", "4", "5+");
-	Stage CurrentStage;
+	private ObservableList<String> YOSlist = FXCollections.observableArrayList("1", "2", "3", "4", "5+");
 	
-	Scene ClubInfoScene;
+	// Stage that holds the current stage
+	private Stage CurrentStage;
 	
-	Scene FinanceScene;
+	// The club profile scene
+	private Scene ClubInfoScene;
 	
-	FinanceProfileController FinanceController;
+	// The finance profile scene
+	private Scene FinanceScene;
 	
-	ClubInfoProfileController ClubController;
+	//The controller for the Finance profile scene
+	private FinanceProfileController FinanceController;
 	
+	// The controller for the club profile scene
+	private ClubInfoProfileController ClubController;
+	
+	// The textfield that contains the information about the users minor
     @FXML
     private TextField minorTextField;
     
-
+    // The textfield that contains the information about the users major
     @FXML
     private TextField majorTextField;
 	
-	
+    // The textfield that contains the information about the users name
 	@FXML
-	public TextField nameField;
-		
-	@FXML
-	public ChoiceBox YOSchoiceBox;
+	private TextField nameField;
 	
+	// Choicebox used for the YOS variable
+	@FXML
+	private ChoiceBox YOSchoiceBox;
+	
+	// The Button Object for the Club profile scene
     @FXML
     private Button ClubInfoButton;
-			
+	
+    // The Button Object for the Finance profile scene
     @FXML
     private Button financeButton;
     
-	@FXML
-	void nameFieldPressed(ActionEvent event) {
-		
-		String strName = nameField.getText();
-		
-	}
 	
-	@FXML
-    void majorPressed(ActionEvent event) {
-		
-		String strMajor = majorTextField.getText();
-    }
-	
-	@FXML
-    void minorPressed(ActionEvent event) {
-		
-		String strMinor = minorTextField.getText();
-	
-    }
-	
+	/**
+	 * This method is used to return a MasterPerson Object to the main controller
+	 * 
+	 * @return MasterPerson - returns a MasterPerson from the information in this scene
+	 */
 	public MasterPerson getMasterPerson()
 	{
 		return new MasterPerson(nameField.getText(), 1, majorTextField.getText(), minorTextField.getText());
 	}
 	
+	/**
+	 * This method is called when the Profile scene is created, the method initializes the values for the ChoiceBox,
+	 * and loads the scenes for the FinanceProfile and ClubProle
+	 * @throws IOException - Potential IOException that may occur when loading the scene
+	 */
 	@FXML
-	private void initialize() throws Exception{
+	private void initialize() throws IOException{
+		
+		//Initializes the Choice by setting the text and values
 		YOSchoiceBox.setValue("Select");
 		YOSchoiceBox.getItems().removeAll(YOSchoiceBox.getItems());
 		YOSchoiceBox.getItems().addAll("1", "2", "3", "4", "5+");
 		
+		//Loads the ClubInfoProfile scene and creates the ClubController
         FXMLLoader loader = new FXMLLoader(getClass().getResource("/PROFILE/ClubInfoProfile.fxml"));
         Parent root = loader.load();
         ClubController = loader.<ClubInfoProfileController>getController();
+        
+        // Reads the interest mapping using ReadFile, this mapping is used in the ClubInfo Scene
         ReadFile reader = new ReadFile("map.txt");
         HashMap<String,String> map = reader.readMap();
         ArrayList<String> keys = reader.getKeys();
+        
+        // Loops through the interest mapping and adds the interest into the ClubInfo Scene
         for(String key : keys) 
         {
         	ClubController.createInterest(key, map.get(key));
         }
         ClubInfoScene = new Scene(root);
         
+        //Creates the FinanceScene and the FinanceController
         FXMLLoader FinanceLoader = new FXMLLoader(getClass().getResource("/PROFILE/FinanceProfile.fxml"));
         Parent FinanceRoot = FinanceLoader.load();
         FinanceController = FinanceLoader.<FinanceProfileController>getController();
@@ -108,13 +117,25 @@ public class ProfileController {
 
 	}
 	
+    /**
+     * Setter method that is used to pass in the previous stage
+     * 
+     * @param c - The stage that the caller is passing in to the controller
+     */
 	public void setStage(Stage c) 
 	{
 		CurrentStage = c;
 	}
 	
+	/**
+	 * This method handles the event when the ClubInfoButton is clicked, the method changes the scene to the
+	 * ClubInfoProfile scene
+	 * 
+	 * @param event - ActionEvent that is passed in as a parameter when the financeButton is pressed
+	 */
+	
 	@FXML
-	void ClubInfoButtonOnClick(ActionEvent event)throws Exception {
+	public void ClubInfoButtonOnClick(ActionEvent event)throws Exception {
         //controller.setStage(CurrentStage);
 		ClubController.setStage(CurrentStage);
 		ClubController.setPreviousScene(nameField.getScene());
@@ -122,20 +143,42 @@ public class ProfileController {
         CurrentStage.show();
 	}
 	
+	/**
+	 * This method is used to return a ClubPerson Object to the main controller so that it can be used
+	 * in the Club Finder 
+	 * 
+	 * @return ClubPerson - returns an instance ClubPerson from the ClubInforProfile scene
+	 */
+	
 	public ClubPerson getClubPerson() 
 	{
 		return new ClubPerson(nameField.getText(), 1, majorTextField.getText(), minorTextField.getText(), ClubController.getInterests(),  ClubController.getFaculties());
 	}
 	
+	/**
+	 * This method handles the event when the financeButton is clicked, the method changes the scene to the
+	 * FinanceProfile scene
+	 * 
+	 * @param event - ActionEvent that is passed in as a parameter when the financeButton is pressed
+	 */
 	@FXML
-    void financeButtonOnClick(ActionEvent event) {
+    private void financeButtonOnClick(ActionEvent event) {
+		
+		//Changes the scene
 		CurrentStage.setScene(FinanceScene);
+		
+		//initializes values for the scene via the FinanceController
 		FinanceController.setMasterPerson(new MasterPerson(nameField.getText(), 1, majorTextField.getText(), minorTextField.getText()));
 		FinanceController.setPreviousScene(nameField.getScene());
 		FinanceController.setStage(CurrentStage);
 		CurrentStage.show();
     }
 	
+	/**
+	 * This method is used to return a FinancePerson Object to the main controller
+	 * 
+	 * @return FinancePerson - returns a FinanacePerson from the FinanceProfile scene
+	 */
 	public FinancePerson getFinancePerson() 
 	{
 		FinanceController.setMasterPerson(new MasterPerson(nameField.getText(), 1, majorTextField.getText(), minorTextField.getText()));
