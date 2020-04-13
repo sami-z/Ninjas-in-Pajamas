bd6059bcbb533464d8b310adb6e2ab175987c32f
Camille Paton <camille.paton@ucalgary.ca>
2020-04-13 16:16:32 -0600

    Documentation for ReadFile

--- main/src/Backend/ReadFile.java
+++ main/src/Backend/ReadFile.java
@@ -5,18 +5,31 @@ import java.util.*;
 
 public class ReadFile {	
 	
-	// 
+	// The file to be read
 	private String dataFile;
+	
+	/** Read File constructor
+	 * 
+	 * @param dataFile - The file to be read
+	 */
 	public ReadFile(String dataFile) 
 	{
 		this.dataFile = dataFile;
 	}
 
+	/** setDataFile(String dataFile)
+	 * Sets the file to be read
+	 * @param dataFile - The file to be read
+	 */
 	public void setDataFile(String dataFile) 
 	{
 		this.dataFile = dataFile;
 	}
 
+	/** getFileReader()
+	 * Returns a BufferedReader which will be used to read the file
+	 * @return readB - a BufferedReader object
+	 */
 	private BufferedReader getFileReader() 
 	{
 		FileReader fileR;
@@ -33,6 +46,10 @@ public class ReadFile {
 		return readB;
 	}
 
+	/** readClubData()
+	 * Returns a ClubList object with all of the clubs read from the file
+	 * @return ret - a ClubList object that contains all of the clubs read from the file
+	 */
 	public ClubList readClubData()
 	{
 
@@ -64,6 +81,10 @@ public class ReadFile {
 		return ret;
 	}
 
+	/** readMap()
+	 * Returns a Hash Map containing the mapping of club categories/interests to their respective numbers
+	 * @return newMap - a Hashmap<String,String> object containing the mapping 
+	 */
 	public HashMap<String,String> readMap()
 	{
 		HashMap<String,String> newMap = new HashMap<String,String>();
@@ -94,6 +115,10 @@ public class ReadFile {
 
 	}
 	
+	/** getKeys()
+	 * Returns an Array List containing the numbers (keys) which represent each club category/interest
+	 * @return keys - an ArrayList<String> object that contains the numbers (keys)
+	 */
 	public ArrayList<String> getKeys()
 	{
 		ArrayList<String> keys = new ArrayList<String>();
@@ -121,6 +146,9 @@ public class ReadFile {
 		return keys;
 	}
 
+	/** displayMap()
+	 * Displays the information contianed in the file using the BufferedReader, line by line
+	 */
 	public void displayMap() 
 	{
 		BufferedReader readB = getFileReader();
